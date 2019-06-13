899ade8eff643b728db8c3e7690ebd24d2292c9e
hika <hika@bsidesoft.com>
2019-06-13 16:51:14 +0900

    .

--- lib/src/main/kotlin/chela/kotlinJS/regex/reStyle.kt
+++ lib/src/main/kotlin/chela/kotlinJS/regex/reStyle.kt
@@ -1,5 +1,5 @@
 package chela.kotlinJS.regex
 
 object reStyle: ChRegex("""-[a-z]"""){
-    fun key(v:String):String = re.replace(v){it.groupValues[0].toUpperCase()}
+    fun key(v:String):String = re.replace(v){it.groupValues[0].substring(1).toUpperCase()}
 }
